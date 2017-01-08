8b5761a432c935b93a1708722943cf55e16f16a7
RedBrick Software <bryced@redbricksoftware.com>
2017-01-08 14:32:45 -0500

    enable testing

---
+++ test/test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var expect = require('chai').expect;
+//var converter = require('../index');
+
+describe('#converter', function() {
+
+    it('should have testing enabled', function() {
+        var result = true;
+        expect(result).to.equal(true);
+    });
+
+});

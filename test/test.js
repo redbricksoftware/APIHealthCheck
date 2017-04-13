490e2a7a70fd7d1950117c089dbcdb577d679384
RedBrick Software <bryced@redbricksoftware.com>
2017-04-13 16:54:56 -0400

    testing

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('test1', function () {
+    describe('anothertest', function () {
+        it('should return -1', function () {
+            assert.equal(-1, -1);
+        });
+    });
+});
---
+++ test/test.ts
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('test1', function () {
+    describe('anothertest', function () {
+        it('should return -1', function () {
+            assert.equal(-1, -1);
+        })
+    })
+});
